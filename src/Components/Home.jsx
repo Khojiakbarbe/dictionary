639ff80753315d78a5409aefaa345ea86edc193b
Khojiakbarbe <121534562+Khojiakbarbe@users.noreply.github.com>
2023-05-02 23:56:11 +0500

    smth change

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -54,12 +54,12 @@ export default function Home() {
 
     return (
         <div className="container p-5" >
-            <div class="form-switch">
-
-                <input onClick={() => backgroundColor()} class="form-check-input" style={bodyColor == 'dark' ? { backgroundColor: '#A445ED' } : { backgroundColor: '#757575' }} type="checkbox" role="switch" id="flexSwitchCheckChecked" />
+            <div class="form-switch mb-3">
+                <label class="form-check-label" for="flexSwitchCheckChecked" style={{marginRight:'5%'}}>{ bodyColor == 'white' ? "Light" : "Dark" }</label>
+                <input onClick={() => backgroundColor()} class="form-check-input" style={bodyColor == 'dark' ? { backgroundColor: '#A445ED' } : { backgroundColor: '#757575' } } type="checkbox" role="switch" id="flexSwitchCheckChecked" />
             </div>
             <input type="text" className="form-control w-50" style={{ display: 'inline-block' }} value={word} onChange={(e) => setWord(e.target.value)} />
-            <button  onClick={search} style={{ position: "relative", right: "5%", border:'none' }}>
+            <button onClick={search} style={{ position: "relative", right: "5%", border: 'none' }}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                 </svg>
